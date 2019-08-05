dc8db4886c4a6aa53855f862eba1572041eeb685
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-08-05 13:10:24 -0700

    schema updated, pulls from db

--- src/schema.js
+++ src/schema.js
@@ -10,13 +10,25 @@ const {
 const db = require('./pg.js');
 
 const getOrders = async () => {
-  const query = `SELECT * FROM orders`;
-
   const client = await db.pool.connect();
+
+  const query = `SELECT * FROM orders;`;
 
   try {
     const results = await client.query(query);
-    if (results.rows.length === 0) return [];
+    return results.rows;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getPayments = async () => {
+  const client = await db.pool.connect();
+
+  const query = `SELECT * FROM payments;`;
+
+  try {
+    const results = await client.query(query);
     return results.rows;
   } catch (err) {
     console.error(err);
@@ -38,6 +50,24 @@ const OrderType = new GraphQLObjectType({
   }),
 });
 
+const PaymentType = new GraphQLObjectType({
+  name: 'Payment',
+  description: 'A payment',
+
+  fields: () => ({
+    id: { type: GraphQLString },
+    amount: { type: GraphQLString },
+    appliedAt: {
+      type: GraphQLString,
+      resolve: (payment) => payment.applied_at,
+    },
+    orderId: {
+      type: GraphQLString,
+      resolve: (payment) => payment.order_id,
+    },
+  }),
+});
+
 const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: '...',
@@ -46,6 +76,10 @@ const QueryType = new GraphQLObjectType({
     orders: {
       type: new GraphQLList(OrderType),
       resolve: () => getOrders(),
+    },
+    payments: {
+      type: new GraphQLList(PaymentType),
+      resolve: () => getPayments(),
     },
   }),
 });
