83d88bff8c711a897e58404ae79707ee3eef9e08
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-08-05 11:28:33 -0700

    update schema with mock data

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -16,3 +16,10 @@ CREATE TABLE payments (
   order_id BIGINT,
   FOREIGN KEY (order_id) REFERENCES orders (id)
 );
+
+INSERT INTO orders(description, total, balance_due) VALUES('test order 1', 80.40, 40.40) RETURNING *;
+INSERT INTO payments(amount, applied_at, order_id) VALUES('40', 'NOW', 1);
+INSERT INTO orders(description, total, balance_due) VALUES('test order 2', 80.40, 80.40) RETURNING *;
+INSERT INTO orders(description, total, balance_due) VALUES('test order 1', 80.40, 20.40) RETURNING *;
+INSERT INTO payments(amount, applied_at, order_id) VALUES('20', 'Two days ago', 3);
+INSERT INTO payments(amount, applied_at, order_id) VALUES('40', 'YESTERDAY', 3);
