f8580506a0a690629592dc26f9e0518f21264c8a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-08-06 15:29:33 -0700

    added constraints to SQL db

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -2,18 +2,18 @@ DROP TABLE IF EXISTS payments;
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE orders (
-  id SERIAL PRIMARY KEY,
-  description TEXT,
-  total NUMERIC,
-  balance_due NUMERIC
+  id SERIAL PRIMARY KEY UNIQUE,
+  description TEXT NOT NULL,
+  total NUMERIC NOT NULL,
+  balance_due NUMERIC NOT NULL
 );
 
 CREATE TABLE payments (
-  id SERIAL PRIMARY KEY,
-  amount NUMERIC,
-  applied_at VARCHAR(63),
+  id SERIAL PRIMARY KEY UNIQUE,
+  amount NUMERIC NOT NULL,
+  applied_at VARCHAR(63) NOT NULL,
   note TEXT,
-  order_id BIGINT,
+  order_id BIGINT NOT NULL,
   FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
